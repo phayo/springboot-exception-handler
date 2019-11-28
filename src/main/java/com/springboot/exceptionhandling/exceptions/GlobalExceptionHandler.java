1ff0c19bda23276dbaeeec639275a98547887cf5
phayo <anazodogodwin@gmail.com>
2019-11-28 18:29:41 +0100

    catch exception using SimpleMappingExceptionHandler

--- src/main/java/com/springboot/exceptionhandling/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/exceptionhandling/exceptions/GlobalExceptionHandler.java
@@ -1,25 +1,25 @@
-package com.springboot.exceptionhandling.exceptions;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
-
-
-@ControllerAdvice
-public class GlobalExceptionHandler {
-    @ExceptionHandler(value={java.lang.ArithmeticException.class})
-    public ModelAndView handlerArithmeticException(Exception e) {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("exception", e.toString());
-        modelAndView.setViewName("mathError");
-        return modelAndView;
-    }
-
-    @ExceptionHandler(value={java.lang.NullPointerException.class})
-    public ModelAndView handlerNullPointerException(Exception e) {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("exception", e.toString());
-        modelAndView.setViewName("nullPointerError");
-        return modelAndView;
-    }
-}
+//package com.springboot.exceptionhandling.exceptions;
+//
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//
+//@ControllerAdvice
+//public class GlobalExceptionHandler {
+//    @ExceptionHandler(value={java.lang.ArithmeticException.class})
+//    public ModelAndView handlerArithmeticException(Exception e) {
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.addObject("exception", e.toString());
+//        modelAndView.setViewName("mathError");
+//        return modelAndView;
+//    }
+//
+//    @ExceptionHandler(value={java.lang.NullPointerException.class})
+//    public ModelAndView handlerNullPointerException(Exception e) {
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.addObject("exception", e.toString());
+//        modelAndView.setViewName("nullPointerError");
+//        return modelAndView;
+//    }
+//}
