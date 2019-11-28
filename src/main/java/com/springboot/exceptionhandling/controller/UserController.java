df89a567c43a370769dbdac7c0670ae2b250dd46
phayo <anazodogodwin@gmail.com>
2019-11-28 18:23:02 +0100

    edit UserController comment ExceptionHandler methods

--- src/main/java/com/springboot/exceptionhandling/controller/UserController.java
+++ src/main/java/com/springboot/exceptionhandling/controller/UserController.java
@@ -21,19 +21,19 @@ public class UserController {
         return "update";
     }
 
-    @ExceptionHandler(value={java.lang.ArithmeticException.class})
-    public ModelAndView handlerArithmeticException(Exception e) {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("exception", e.toString());
-        modelAndView.setViewName("mathError");
-        return modelAndView;
-    }
-
-    @ExceptionHandler(value={java.lang.NullPointerException.class})
-    public ModelAndView handlerNullPointerException(Exception e) {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("exception", e.toString());
-        modelAndView.setViewName("nullPointerError");
-        return modelAndView;
-    }
+//    @ExceptionHandler(value={java.lang.ArithmeticException.class})
+//    public ModelAndView handlerArithmeticException(Exception e) {
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.addObject("exception", e.toString());
+//        modelAndView.setViewName("mathError");
+//        return modelAndView;
+//    }
+//
+//    @ExceptionHandler(value={java.lang.NullPointerException.class})
+//    public ModelAndView handlerNullPointerException(Exception e) {
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.addObject("exception", e.toString());
+//        modelAndView.setViewName("nullPointerError");
+//        return modelAndView;
+//    }
 }
